f72b6f684c09eb1bd5c81f1f88a206b8997351b5
thomas <tom.angotti11@gmail.com>
2023-12-23 11:36:04 -0700

    basic structure set up

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Home from './components/Home/Home';
+import ForgotPassword from './components/forgotPassword/ForgotPassword';
+import AdminLogin from './components/AdminLogin/AdminLogin';
+
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/admin-login" element={<AdminLogin />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/AdminLogin/AdminLogin.js
@@ -0,0 +1,12 @@
+
+
+const AdminLogin = () => {
+    return(
+        <div>
+            <h1>Admin Login</h1>
+        </div>
+    )
+}
+
+
+export default AdminLogin
